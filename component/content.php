052384216ea080098731abf6b47fe7ea9d345f30
unknown <asholehuddin0@gmail.com>
2024-02-19 16:27:30 +0700

    portofolio

---
+++ component/content.php
@@ -0,0 +1,19 @@
+<?php include_once('head.php') ?>
+
+<main>
+    <div class="d-flex justify-content-center bg-primary  ">
+            <div class="d-flex container-flex-sm-row ">
+            <section>
+                <img src="img/pas-foto.png" alt="" class="bg-black img-fluid sm ">
+            </section>
+            <section class="position-relative adjust-ul">
+                <ul class="bg-black text-danger remove-dot">
+                    <li><p class="hello">Hello</p></li>
+                    <li><p class="name">I'm Muhammad Irvan Taufiqurrahman</p></li>
+                    <li><p class="desc">lorem ipsum bla bla bla bla blas</p></li>
+                    <button type="button" class="btn-download">Download CV</button>
+                </ul>
+            </section>
+            </div>
+    </div>
+</main>
---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include('component/navbar.php') ?>
+<body>
+<?php include('component/content.php') ?>
+    
+</body>
+</html>
