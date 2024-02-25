f236a700273b4347dc07e38ef457b483cfd53d81
unknown <asholehuddin0@gmail.com>
2024-02-25 22:28:00 +0900

    vercel

--- api/content.php
+++ api/content.php
@@ -1,4 +1,4 @@
-<?php include_once('head.php') ?>
+<?php include_once('/../../../../head.php') ?>
 <body class="body">
 <main class="bg-home">
     <div class="d-flex justify-content-center " id="home-nav">
