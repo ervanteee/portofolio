08268ffafd226f439bf4fa8bc8df597db9a64b10
unknown <asholehuddin0@gmail.com>
2024-02-25 22:16:13 +0900

    parsel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,18 @@
 <?php 
-require_once('component/content.php');
+header('Content-Type: application/json');
+
+if($_SERVER['REQUEST_METHOD'] === 'GET'){
+
+    $response = array(
+        'message' => 'Hello, World'
+    );
+
+    echo json_encode($response);
+}else{
+    http_response_code(405);
+    echo json_encode(array('error' => 'Method Not Allowed'));
+}
+
+include('component/content.php');
 
 ?>
