a3c776c661df940598003e04cedc9fbc77818463
unknown <asholehuddin0@gmail.com>
2024-02-25 22:25:39 +0900

    fix

--- api/index.php
+++ api/index.php
@@ -1,17 +1,4 @@
 <?php 
-header('Content-Type: application/json');
-
-if($_SERVER['REQUEST_METHOD'] === 'GET'){
-
-    $response = array(
-        'message' => 'Hello, World'
-    );
-
-    echo json_encode($response);
-}else{
-    http_response_code(405);
-    echo json_encode(array('error' => 'Method Not Allowed'));
-}
 
 include('content.php');
 
